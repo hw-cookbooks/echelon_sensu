e5b37880f958df93af5de234d361ee3b9cd245db
Chris Roberts <chrisroberts.code@gmail.com>
2012-07-08 08:52:15 -0700

    Remove sensu-plugin gem install from client recipe

--- recipes/client.rb
+++ recipes/client.rb
@@ -1,8 +1,5 @@
 include_recipe 'echelon_sensu::default'
 
-sensu_gem 'sensu-plugin' do
-  action :upgrade
-end
 include_recipe "sensu::client"
 
 remote_directory File.join(node.sensu.directory, "plugins") do

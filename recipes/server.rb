47b33af91dce398d9a3e7799844a57da67e4678a
Chris Roberts <chrisroberts.code@gmail.com>
2012-07-09 09:40:46 -0700

    Clean up chef config file generation. Ensure sensu can read signing key.

--- recipes/server.rb
+++ recipes/server.rb
@@ -41,32 +41,37 @@ rescue => e
   Chef::Log.warn "No campfire config found for sensu handler (#{e.class}: #{e})"
 end
 
-begin
-  [:server_url, :validation_client_name, :conf_dir].each do |attr|
-    if node[:echelon_sensu][attr].nil?
-      node[:echelon_sensu][attr] = node[:chef_client][attr]
-    end
+config_args = {}
+if(node[:recipes].include?('chef-client::config'))
+  config_args[:enabled] = true
+  [:server_url, :conf_dir].each do |attr|
+    config_args[attr] = node[:echelon_sensu][attr] || node[:chef_client][attr]
   end
-  if node[:echelon_sensu][:api_ip_addr].nil?
-    node[:echelon_sensu][:api_ip_addr] = node[:sensu][:redis][:host]
-  end
-rescue
+  config_args[:api_ip_addr] = node[:echelon_sensu][:api_ip_addr] || node[:sensu][:redis][:host]
+  config_args[:signing_key_filename] = node[:echelon_sensu][:signing_key_filename] || File.join(config_args[:conf_dir], 'client.pem')
+else
   Chef::Log.warn "Cookbook chef_client unavailable, echelon_sensu cannot dynamically check nodes"
-  node[:echelon_sensu][:enabled] = false
+  config_args[:enabled] = false
 end
+
+if(File.exists?(config_args[:signing_key_filename]))
+  Chef::Log.warn "Changing group and making readable for sensu: #{config_args[:signing_key_filename]}"
+  file config_args[:signing_key_filename] do
+    group 'sensu'
+    mode 0640
+  end
+end
+
 
 file "/etc/sensu/conf.d/chef.json" do
   mode 0644
   content(
     JSON.pretty_generate(
-      :chef => {
-        :api_ip_addr => node[:echelon_sensu][:api_ip_addr],
-        :server_url => node[:echelon_sensu][:server_url],
-        :validation_client_name => node[:echelon_sensu][:validation_client_name],
-        :conf_dir => node[:echelon_sensu][:conf_dir],
-        :enabled => node[:echelon_sensu][:enabled],
-        :environment => Array(node[:echelon_sensu][:alert_environment])
-      }
+      :chef => config_args.merge(
+        :enabled => node[:echelon_sensu][:enabled] || config_args[:enabled],
+        :environment => Array(node[:echelon_sensu][:alert_environment]),
+        :node_name => Chef::Config[:node_name]
+      )
     )
   )
 end

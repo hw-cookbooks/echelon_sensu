4fb36910c936780a845616dd7c72b609c52c298d
Chris Roberts <chrisroberts.code@gmail.com>
2012-07-09 09:40:26 -0700

    Provide signing key attribute to allow override

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,4 +4,5 @@ default[:echelon_sensu][:validation_client_name] = nil
 default[:echelon_sensu][:conf_dir] = nil
 default[:echelon_sensu][:enabled] = true
 default[:echelon_sensu][:alert_environment] = []
+default[:echelon_sensu][:signing_key_filename] = nil
 default[:echelon_sensu][:init_fixes] = true
